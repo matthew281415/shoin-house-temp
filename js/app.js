0481626186c0ef668619723c3d4f1af589e851c2
matthew281415 <lucaxtet204@gmail.com>
2023-08-18 10:52:05 +0630

    12-8-2023

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("hi");
+//start jquery area
+$(document).ready(function(){
+   $("#banner_carousel").owlCarousel({
+    items:1,
+    loop:true,
+    dots:false,
+    // autoplay:true,
+   });
+
+   $(".product_edt_btn").click(function(){
+      //onsole.log($(this));
+      $(this).toggleClass("active");
+      //console.log($(this).parent()); //result = product_main_container
+      //console.log($(this).parent().find(".product_name_group"))
+      $(this).parent().find(".product_name_group").toggleClass("active");
+      //console.log($(this).parent().find(".product_edit_form"));
+      $(this).parent().find(".product_edit_form").toggleClass("active");
+      
+   })
+   
+})
+//end jquery
