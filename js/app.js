51856ce4070f2400ee2c9450d8cae33880fddea1
matthew281415 <lucaxtet204@gmail.com>
2023-08-19 11:41:48 +0630

    19-8-2023

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 //start jquery area
+$(window).on("load",function(){
+   $("#preloader").delay(500).fadeOut("slow");
+})
 $(document).ready(function(){
    $("#banner_carousel").owlCarousel({
     items:1,
@@ -16,6 +19,58 @@ $(document).ready(function(){
       //console.log($(this).parent().find(".product_edit_form"));
       $(this).parent().find(".product_edit_form").toggleClass("active");
       
+   })
+
+   $(".search_btn").click(function(){
+      $("#search_modal").addClass("active");
+   })
+
+   $(window).click(function(e){
+         console.log(e.target);
+         if(e.target.classList.contains("search_modal")){
+            $("#search_modal").removeClass("active");
+         }
+   })
+
+   $(window).scroll(function(e){
+      //console.log($(this).scrollTop());
+      var getscrollamount=$(this).scrollTop();
+      console.log(getscrollamount);
+      if(getscrollamount > 300){
+         $(".navbar").addClass("active");
+
+      }else{
+         $(".navbar").removeClass("active");
+      }
+
+      if(getscrollamount > 900){
+         $(".arrival_content_container").addClass("active");
+
+      }else{
+         $(".arrival_content_container").removeClass("active");
+      }
+
+      if(getscrollamount > 1850){
+         $(".about_title_container").addClass("active");
+
+      }else{
+         $(".about_title_container").removeClass("active");
+      }
+
+      if(getscrollamount > 2380){
+         $(".established_content_container").addClass("active");
+
+      }else{
+         $(".established_content_container").removeClass("active");
+      }
+      if(getscrollamount > 2900){
+         $(".our_story_section").addClass("active");
+
+      }else{
+         $(".our_story_section").removeClass("active");
+      }
+
+
    })
    
 })
